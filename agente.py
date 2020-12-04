f8656e2c5c0fea54f1bce409812f3882d6762134
ved-suiciniv <denis.vini@hotmail.com>
2020-12-04 20:58:46 +0800

    teste1

---
+++ agente.py
@@ -0,0 +1,43 @@
+"""
+agente.py
+
+criar aqui as funções que respondem às perguntas
+e quaisquer outras que achem necessário criar
+
+colocar aqui os nomes e número de aluno:
+40681, Vinicius Rodrigues Silva Costa
+NUM2, NOME2
+
+"""
+import time
+print('Vincius\nTime: ',time.time())
+list = []
+def work(posicao, bateria, objetos):
+    resp1(objetos)
+
+		
+def resp1(objetos):
+    if objetos !=[] and objetos not in list:
+        list.append(objetos)
+        print('ola',list)
+	
+def resp2():
+    pass
+
+def resp3():
+	pass
+	
+def resp4():
+    pass
+
+def resp5():
+    pass
+
+def resp6():
+    pass
+
+def resp7():
+    pass
+
+def resp8():
+    pass
