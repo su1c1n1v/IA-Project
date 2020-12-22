ead68fafcff172108f622aa627f62916036ac8d5
ved-suiciniv <denis.vini@hotmail.com>
2020-12-23 07:07:50 +0800

    Feature add room in the graph during the life (if the room it is not in the graph)

--- graph.py
+++ graph.py
@@ -16,27 +16,26 @@ graph = net.Graph()
 	#print(graph.edges()) #printa todos os vertices
 	#print(graph.nodes()) #printa todos os nodos
 
-def initGraph():
-
-	
-	# ---------------------- #
+def init_graph():
 	graph.add_edge('Corredor 1','Corredor 2',weight=[[180,30],[180,70]])
 	graph.add_edge('Corredor 1','Escadas',weight=[[180,30],[180,70]])
 	graph.add_edge('Corredor 1','Corredor 3',weight=[[540,30],[540,135]])
 	graph.add_edge('Corredor 2','Corredor 4',weight=[[30,305],[85,305]])
 	graph.add_edge('Corredor 3','Corredor 4',weight=[[565,305],[635,305]])
+# --------------------------------------------------------------------- #
 	
+
+def searchRoomInGraph(localList,weight):
+	if localList[-1] not in graph:
+		graph.add_edge(localList[-1],localList[-2],weight=[weight])
+		localList.clear()
 	
-	'''
-	# ------como acessar os pesos------ #
+def showEdges():
+	sp = dict(net.all_pairs_shortest_path(graph))
 	print('Grafo ',graph.edges())
-	print('Grafo ',graph['Corredor 1']['Corredor 2']['weight'])
 	for node in graph.nodes():
 		print('|',node,'--------|')
 		for edge in graph[node]:
-			print('Edge: ',edge,'\nPeso:',graph[node][edge]['weight'])
-		print('|------------|')
-		
-	'''
-	
-# ------------------------------------------------------ #		
+			print('Edge: ',edge,'\nPeso:',graph[node][edge]['weight'],'\nShortest:',sp[node][edge])
+		print('|------------------|')
+# --------------------------------------------------------------------- #		
