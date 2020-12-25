8432e7dbc6e57e1c2b18ec51b2d5b50b73317c93
Frias <miguelfrias16@gmail.com>
2020-12-25 21:50:51 +0000

    ok

---
+++ questions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print(' ***** ')
+print('1-Qual foi a penultima pessoa que viste?')
+print('2-Em que tipo de sala estas agora?')
+print('3-Qual é o caminho para a sala de enfermeiros mais próxima?')
+print('4-Qual a distancia até ao médico mais próximo?')
+print('5-Quanto tempo achas que demoras a ir de onde estás até às escadas?')
+print('6-Quanto tempo achas que falta até ficares sem bateria?')
+print('7-Qual a probabilidades de encontrar um livro numa divisão, se já encontraste uma cadeira?')
+print('8-Se encontrares um enfermeiro numa divisão, qual é a probabilidade de estar lá um doente?')
+
