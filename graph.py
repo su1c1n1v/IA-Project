513e4b71e7793d3ec5b1cfb63ed409bcac0a7399
ved-suiciniv <denis.vini@hotmail.com>
2021-01-02 04:20:48 +0800

    feature explanation of the functions, question 7 done

--- graph.py
+++ graph.py
@@ -1,24 +1,36 @@
 import time
 import math
 import networkx as net
+
+#Nesse ficheiro é onde manipulamos os grafos, e suas respectivas 
+#funcionalidades. Aqui adicionamos nodos ao grafo, adicionamos um vertice
+#entre dois nodos, procuramos o menor caminho entre dois nodos e o tempo
+#para percorrer este caminho.
 	
 # add_node ------------------------------------------------------------ #
 def add_nodes(node1,node2,points,graph):
+	#Adiciona um vertice 'node1' com o 'node2' 
 	graph.add_edge(node1,node2,limits=points[:],weight=0)
 # --------------------------------------------------------------------- #
 # Search_Room --------------------------------------------------------- #
 def search_room_in_graph(localList,weight,graph):
+	#Verifica se o node em que o agente estar atualmente já esta conectada	
+	#com outro vertice dentro do grafo
 	if len(localList) > 1:
 		if not graph.has_edge(localList[-1],localList[-2]):
 			add_nodes(localList[-1],localList[-2],weight,graph)
 # --------------------------------------------------------------------- #
 # Add obj in the graph in it position --------------------------------- #
 def add_obj_graph(localList,obj,weight,graph):
+	#Adiciona um vertice entre um 'obj' com a posiçao atual do agente
+	#(que seria o node atual)
 	if not graph.has_edge(localList[-1],obj):
+		print('Adicionado',obj,localList[-1])
 		add_nodes(localList[-1],obj,weight,graph)
 # --------------------------------------------------------------------- #
 # Show all edges ------------------------------------------------------ #
 def show_edges(graph):
+	#Mostra na linha de comando todos os edges já adicionado no grafo (debug)
 	sp = dict(net.all_pairs_dijkstra_path(graph))
 	print('Grafo ',graph.edges())
 	for node in graph.nodes():
@@ -26,14 +38,38 @@ def show_edges(graph):
 		for edge in graph[node]:
 			print('	Edge:',edge,graph[node][edge])
 # --------------------------------------------------------------------- #
-# The new shortest path ----------------------------------------------- #
+# The shortest path --------------------------------------------------- #
 def create_graph_distance_two_nodes(position,local_points,local,graph):
+	#Função central para a manipulação do grafo (Questões 3, 4 e 5)
+	
+	#Esta função ajuda para encontrar o menor caminho entre dois nodos, e
+	#para isso é criado um multigrafo (permite fazer dois nodos se 
+	#conectarem mais de uma vez) e com ele é criado vertices que 
+	#representam a distancia entre dois nodos mas levando em conta o 
+	#destino, ou seja é quardado o 'weight' dos dois nodos levando em 
+	#conta de onde eu estou para onde estou indo.
+	#Exemplo: 
+	#node1 (corredor 1)
+	#node2 (corredor 2)
+	#Vertice1 (node1,node2,weight=100,indo=Escadas)
+	#Vertice2 (node1,node2,weight=300,indo=Corredor 3)
+	#Vertice3 (node1,node2,weight=200,indo=Sala 5)
+	#Ou seja, dependedo do meu objetivo de destino o peso 'weight' tem
+	#diferentes valores, com isso podemos saber qual e o menor caminho.
+	
+	#Parte 1:
+	#Nesta parte não e criado vertices auxiliares pois, o nodo manipulado 
+	#aqui e o nodo onde o agente estar, ou seja so e dado a distancia do
+	#agente até as saidas do nodo.
 	graph2 = graph.copy()
 	for node in graph2[position[-1]]:
 		point1 = graph2[position[-1]][node][0]['limits']
 		point2 = local_points[0]							
 		cost = distance_two_points(point1,point2)
 		graph2[position[-1]][node][0]['weight'] = cost	
+	#Parte 2:
+	#Nesta parte que e feito os vertices auxiliares em todos os nodos
+	#e suas saidas e objetivos
 	for node in graph2.nodes():
 		if node != position[-1]:
 			for edge1 in graph2[node]:
@@ -43,25 +79,36 @@ def create_graph_distance_two_nodes(position,local_points,local,graph):
 						if(edge2 != edge1 and edge1 != position[-1]):
 							point2 = graph2[node][edge2][0]['limits']	
 							cost = distance_two_points(point1,point2)
-							graph2.add_edge(node,edge2,limit=point2,weight=cost,comming=edge1)
+							graph2.add_edge(node,edge2,limit=point2,weight=cost,going=edge1)
 							graph2[node][edge2][0]['weight'] = cost
-							graph2[node][edge2][0]['comming'] = edge1
+							graph2[node][edge2][0]['going'] = edge1
+	#Apos ser criado o multigrafo já com todos os vertices auxiliares e 
+	#enviado para 'search_path_two_nodes' que vai encontrar o menor path
 	return search_path_two_nodes(graph2,local_points,position,local)
 # --------------------------------------------------------------------- #
 # Search the best path for two nodes ---------------------------------- #
 def search_path_two_nodes(graph,local_points,position,local):
+	#Função auxiliar que ajuda a encontra o menor path entre dois nodos
+	#para isso ser feito e utilizado a função 'all_pairs_dijkstra_path'
+	#da biblioteca networkX, que tem a funcionalidade de aplicar o teorema
+	#de dijkstra.
 	for node in graph[position[-1]]:
 		point1 = graph[position[-1]][node][0]['limits']
 		point2 = local_points[0]							
 		cost = distance_two_points(point1,point2)
 		graph[position[-1]][node][0]['weight'] = cost
-		graph[position[-1]][node][0]['comming'] = 'local'
+		graph[position[-1]][node][0]['going'] = 'local'
 	sp = dict(net.all_pairs_dijkstra_path(graph))
 	path = sp[position[-1]][local]
+	#Apos ser encontrado o path, e enviado para 'distance_two_nodes' que
+	#vai ajudar a somar a distancias entre esse path e o tempo que o agente
+	#levará para percorrer (o tempo é uma media da velocidade por segundo).
 	return distance_two_nodes(graph,path)
 # --------------------------------------------------------------------- #
 # Distance between two nodes ------------------------------------------ #
 def distance_two_nodes(graph,path):
+	#Função auxiliar que ajudara a somar os 'weight' de cada vertice do
+	#path dado na função.
 	distance = 0
 	for pos in range(len(path)):
 		if(pos+1 < len(path)):
@@ -69,19 +116,24 @@ def distance_two_nodes(graph,path):
 				distance = distance + graph[path[pos]][path[pos+1]][0]['weight']
 				if(len(path) == 3):
 					for keys in graph[path[pos]][path[pos+1]]:
-						if(graph[path[pos]][path[pos+1]][keys]['comming'] == path[pos+2]):
+						if(graph[path[pos]][path[pos+1]][keys]['going'] == path[pos+2]):
 							distance = distance + graph[path[pos]][path[pos+1]][keys]['weight']
 							break
 					return [distance,distance/245.6,path]
 			if(pos+2 < len(path)):
 				for keys in graph[path[pos]][path[pos+1]]:
-					if(graph[path[pos]][path[pos+1]][keys]['comming'] == path[pos+2]):
+					if(graph[path[pos]][path[pos+1]][keys]['going'] == path[pos+2]):
 						distance = distance + graph[path[pos]][path[pos+1]][keys]['weight']
 						break
 	return [distance,distance/245.6,path]
 # --------------------------------------------------------------------- #
 # The distance between two points ------------------------------------- #		
 def distance_two_points(point1,point2):
+	#Função responsavel por aplicar o teorema de pitagora (a distancia 
+	#entre dois pontos e uma linha reta: X^2 + Y^2 = z^2), recebe dois 
+	#pontos e calcula sua hipotenusa entre esse dois pontos.
+	#O resultado dessa função e colocado nos 'weight' nas funções 
+	#create_graph_distance_two_nodes e search_path_two_nodes.
 	x1 = 0
 	y1 = 0
 	x2 = 0
--- main.py
+++ main.py
@@ -25,8 +25,17 @@ speed = 245.6
 # --------------------------------------------------------------------- #		
 # Get the list of object and put in lists ----------------------------- #		
 def get_objects(object_list,local):
+	#Função mais importante, tem a funcionalidade de pegar os inputs 
+	#recebidos pelo agente e classifica o obj podendo se 'enfermeiro',
+	#'livro','medico','doente'... depois de classificar, é criado um 
+	#vinculo  do objeto com a sala (criado um vertice entre a sala e o 
+	#objeto)
 	for obj in object_list:
+		#Pega a categoria do objeto 
 		cathegory = obj.split("_")[0]
+		#Classifica se o objeto e uma pessoa, se for e adicionado a uma 
+		#lista auxiliar que guarda as duas ultimas pessoas que o agente 
+		#encontrou.
 		if(cathegory == 'enfermeiro' or cathegory == 'medico'or cathegory == 'doente'):
 			if(len(pessoa_list) > 2):
 				pessoa_list.remove(pessoa_list[0])
@@ -35,6 +44,8 @@ def get_objects(object_list,local):
 			else:
 				if(pessoa_list[-1] != obj):
 					pessoa_list.append(obj)
+		#Parte de categorizar o objeto e adiciona o vertice do objeto 
+		#a sala.
 		if(cathegory == 'enfermeiro'):
 			if obj not in enfermeiro_list:
 				enfermeiro_list.append(obj)
@@ -66,11 +77,13 @@ def get_objects(object_list,local):
 # --------------------------------------------------------------------- #	
 # Refresh the position of the machine --------------------------------- #
 def actually_location(pos):
+	#Atualiza a posição do agente com o clock do 'work', a posição é 
+	#determinada pelas paredes das salas que são definidas logo a baixo. 
 	local_point.clear()
 	local_point.append(pos)
 	x = pos[0]
 	y = pos[1]
-	
+	#Pontos que definem os limites de cada sala do software
 	if(x >= 30 and x <= 85 and y >= 90 and y <= 305):
 		position = 'Corredor 2'
 		getPosition(position)
@@ -119,15 +132,24 @@ def actually_location(pos):
 # --------------------------------------------------------------------- #
 # Refresh position  --------------------------------------------------- #		
 def getPosition(pos):
+	#Todo clock do work atualiza a posição do agente e guarda ela na lista
+	#'position'
 	if(pos != position[-1]):
 		position.append(pos)
 # --------------------------------------------------------------------- #
 # Get the position ---------------------------------------------------- #		
 def getActuallyPosition():
+	#Pega a ultima coordenada do agente (a que o agente estar no momento)
+	# e retorna para ser usado no agente.py
 	return position
 # --------------------------------------------------------------------- #	
 # Call the shortest path to 'node' ------------------------------------ #
 def call_shortest_path(node):
+	#Tem o proposito de pegar o menor caminho de onde o agente estar para 
+	#o 'node'
+	
+	#Questão 4: Pega todos os medicos ja vistos, e verifica qual deles 
+	#tem a menor distancia. 
 	if(node == 'medico'):
 		medicos = []
 		if(len(medico_list) >= 1):
@@ -150,9 +172,14 @@ def call_shortest_path(node):
 # --------------------------------------------------------------------- #	
 # Search the category of this room ------------------------------------ #
 def search_room_category():
+	#Questão 2: Categoriza a sala dependedo dos objetos encontrados dentro
+	#da sala atual, para isso e percorrido todos os vertices da sala, e 
+	#verifica se cumpre com os requisitos para ser uma 'Sala de enfermeiros'
+	#, 'Sala de espera' ou 'quarto'.
 	mesa = []
 	cadeira = []
 	cama = []
+	#Conta quantas camas, mesas e cadeira tem na sala
 	for node in graph.edges(position[-1]):
 		category = node[1].split("_")[0]
 		if(category == 'cama'):
@@ -161,6 +188,8 @@ def search_room_category():
 			mesa.append(node[1])
 		elif(category == 'cadeira'):
 			cadeira.append(node[1])
+	#Dependendo do objeto encontrado e determinado que tipo de sala o 
+	#agente estar
 	if(len(cama) >= 1):	
 		if(position[-1] not in quarto_list):
 			quarto_list.append(position[-1])
@@ -184,14 +213,20 @@ def search_room_category():
 # --------------------------------------------------------------------- #	
 # Add Room or Object in the graph ------------------------------------- #
 def refresh_graph(posicao):
+	#Atualiza o grafo, e verifica se o node atual ja possui um vertice 
+	#dentro do grafo, se tiver não faz nada, se não tiver, adiciona um vertice
+	#o node atual com o anterior
 	gph.search_room_in_graph(getActuallyPosition(),posicao,graph)
 # --------------------------------------------------------------------- #
 # Show all edges in the graph ----------------------------------------- #
 def show_edges():
+	#Mostra na linha de comando todos os vertices já adicionados no grafo
 	gph.show_edges(graph)
 # --------------------------------------------------------------------- #
 # Show all rooms and it categories ------------------------------------ #	
 def show_all_rooms_categories():
+	#Mostra na linha de comando todas as salas e suas categorias já 
+	#adicionado no grafo
 	for node in quarto_list:
 		print('Quarto:',node)	
 	for node in sala_de_espera_list:
@@ -201,7 +236,11 @@ def show_all_rooms_categories():
 # --------------------------------------------------------------------- #
 # The shortest path to the closest 'enfermaria' ----------------------- #
 def shortest_path_enfermaria():
+	#Questão 3: Procura o menor caminho ate a sala de enfermeiro mais 
+	#proxima. 
 	enfermaria = []
+	#Verificamos todas as salas de enfermeiro, e nos certificamos de pegar 
+	#a sala mais proxima
 	if(len(sala_dos_enfermeiros_list) >= 1):
 		for node in sala_dos_enfermeiros_list:
 			path = gph.create_graph_distance_two_nodes(position,local_point,node,graph)
@@ -213,15 +252,74 @@ def shortest_path_enfermaria():
 					enfermaria.append(path)
 		return enfermaria
 	else:
-		return 'None'
+		return 'Not found any "Sala de enfermeiro"!'
 # --------------------------------------------------------------------- #
 # Get the penult person found ----------------------------------------- #
 def get_penult_person():
+	#Questão 1:
+	#Pega a ultima pessoa que o agente viu, que e guardado na 'pessoa_list'
 	if(pessoa_list == []):
 		return 'Not found anyone!'
 	elif(len(pessoa_list) >= 2):
 		return 'Penult: ' + pessoa_list[-2] + '!'
 	else:
-		return 'Only found 1 person!' +'\nPerson: ' + pessoa_list[0]
+		return 'Only found 1 person!' + '\nPerson: ' + pessoa_list[0]
 # --------------------------------------------------------------------- #
-		
+# Questão 7 ----------------------------------------------------------- #
+def question7():
+	#Nesta questão foi feita usando duas maneiras
+	#1º sendo o livro, e a cadeira independentes
+	#2º usando as salas como fator de dependencia entre os objetos, para
+	#ser possivel usar redes  bayesianas
+	
+	#Aqui pegamos a quantidade de todas as salas e objetos para que 
+	#possamos calcular sua probabilidade 
+	total_espera = len(sala_de_espera_list)
+	total_enfermaria = len(sala_dos_enfermeiros_list)
+	total_quarto = len(quarto_list)
+	total_livro = len(livro_list)
+	total_cadeira = len(cadeira_list)
+	
+	total_livro_sala_espera = how_much_obj_has_in_the_room(sala_de_espera_list,'livro')
+	total_livro_quarto = how_much_obj_has_in_the_room(quarto_list,'livro')
+	total_livro_sala_de_enfermeiros = how_much_obj_has_in_the_room(sala_dos_enfermeiros_list,'livro')
+	
+	total_cadeira_sala_espera = how_much_obj_has_in_the_room(sala_de_espera_list,'cadeira')
+	total_cadeira_quarto = how_much_obj_has_in_the_room(quarto_list,'cadeira')
+	total_cadeira_sala_de_enfermeiros = how_much_obj_has_in_the_room(sala_dos_enfermeiros_list,'cadeira')
+	
+	#Resolução livro e cadeira sendo independente ---------------------
+	resposta1 = (total_livro/10)*100
+	
+	#Resolução suando bayseana ----------------------------------------
+	#Probabilidade da sala ser uma "sala de espera", vezes a chance de 
+	#encontrar uma cadeira, vezes a chance de encontrar um livro
+	espera = (total_espera/10) * 1 * (total_livro_sala_espera/total_livro)
+	
+	#Probabilidade da sala ser uma "Sala de enfermeiros" vezes a chance 
+	#de encontrar uma cadeira, vezes a chance de encontrar um livro 
+	#nesta sala
+	enfermaria = (total_enfermaria/10) * 1 * (total_livro_sala_de_enfermeiros/total_livro)
+	
+	#Probabilidade da sala ser um "Quarto" vezes a chance de ter uma 
+	#cadeira, vezes a chance de ter um livro no quarto 
+	quarto = (total_quarto/10) * (total_cadeira/10) * (total_livro_quarto/total_livro)
+	#Resolução ultilizando bayseana
+	reposta2 = (espera + enfermaria + quarto)*100
+	
+	return [resposta1,reposta2]
+# --------------------------------------------------------------------- #
+# Look how much 'obj' has in this room -------------------------------- # 
+def how_much_obj_has_in_the_room(list_obj,obj):
+	#Função auxiliar, que tem o proposito de ajuda a contar quanto objetos
+	#do tipo 'obj' estão interligados atraves de um vertice com a sala atual
+	count = 0
+	for node in list_obj:
+		print('-------------')
+		for edge in graph.edges(node):
+			print(edge)
+			if(edge[0].split("_")[0] == obj):
+				count = count + 1
+			if(edge[1].split("_")[0] == obj):
+				count = count + 1
+	return count
